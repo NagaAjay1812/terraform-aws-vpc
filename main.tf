b706f2d655d3ea8c450edc72bd740d2772ece891
Naga Ajay <nagaajay@Nagas-Mac-mini.local>
2026-03-02 17:30:27 -0500

    “actions”

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ resource "aws_internet_gateway" "main" {
   tags = local.ig_final_tags
 }
 
-# 3. create the  public subnets and 
+# 3. create the  public subnets 
 resource "aws_subnet" "public" {
   count                   = length(var.public_subnet_cidrs)
   vpc_id                  = aws_vpc.main.id
@@ -31,4 +31,39 @@ resource "aws_subnet" "public" {
 
 }
 
+# 3. create the  private subnets 
+resource "aws_subnet" "public" {
+  count                   = length(var.private_subnet_cidrs)
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.private_subnet_cidrs[count.index]
+  availability_zone       = local.az_names[count.index]
+  map_public_ip_on_launch = false # Key setting to auto-assign public IPs
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.project}-${var.environment}-private-${local.az_names[count.index]}}" # roboshop-dev-private-us-east-1a/1b
+    },
+    var.private_subnet_tags
+  )
+
+}
+
+# 3. create the database subnets 
+resource "aws_subnet" "public" {
+  count                   = length(var.database_subnet_cidrs)
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.database_subnet_cidrs[count.index]
+  availability_zone       = local.az_names[count.index]
+  map_public_ip_on_launch = false # Key setting to auto-assign public IPs
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.project}-${var.environment}-database-${local.az_names[count.index]}}" # roboshop-dev-database-us-east-1a/1b
+    },
+    var.database_subnet_tags
+  )
+
+}
 
--- variables.tf
+++ variables.tf
@@ -17,6 +17,15 @@ variable "public_subnet_cidrs" {
   default = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
+variable "private_subnet_cidrs" {
+  type    = list(any)
+  default = ["10.0.11.0/24", "10.0.12.0/24"]
+}
+variable "database_subnet_cidrs" {
+  type    = list(any)
+  default = ["10.0.21.0/24", "10.0.22.0/24"]
+}
+
 
 variable "vpc_tags" {
   type = map(any)
@@ -30,6 +39,18 @@ variable "ig_tags" {
 }
 
 variable "public_subnet_tags" {
+  type = map(any)
+  default = {
+
+  }
+}
+variable "private_subnet_tags" {
+  type = map(any)
+  default = {
+
+  }
+}
+variable "database_subnet_tags" {
   type = map(any)
   default = {
 
