24e0135acbf777b26667833b5b5c488eddd1e90b
Naga Ajay <nagaajay@Nagas-Mac-mini.local>
2026-03-02 22:33:13 -0500

    “actions”

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
 output "azs_info" {
   value = data.aws_availability_zones.available
 }
+
+output "default_vpc_id" {
+  value = data.aws_vpc.default.id
+}
--- peering.tf
+++ peering.tf
@@ -3,7 +3,7 @@ resource "aws_vpc_peering_connection" "default" {
   #peer_owner_id = var.peer_owner_id
 
   #Accepter -  default vpc or opposite connection
-  peer_vpc_id = aws_vpc.default.id
+  peer_vpc_id = data.aws_vpc.default.id
 
   #Requestor -  roboshop-dev vpc 
   vpc_id = aws_vpc.main.id
