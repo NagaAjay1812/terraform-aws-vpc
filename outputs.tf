0f2f0a97da1c1ffda999500bb18d5b6c8cb9003c
Naga Ajay <nagaajay@Nagas-Mac-mini.local>
2026-03-03 17:50:58 -0500

    “actions”

--- outputs.tf
+++ outputs.tf
@@ -3,7 +3,7 @@ output "azs_info" {
 }
 
 output "main_vpc_id" {
-  value = data.aws_vpc.main.id
+  value = aws_vpc.main.id
 }
 
 output "default_vpc_id" {
@@ -11,15 +11,15 @@ output "default_vpc_id" {
 }
 
 output "publc_subnet_ids" {
-  value = data.aws_subnet.public[*].id
+  value = aws_subnet.public[*].id
 }
 
 output "private_subnet_ids" {
-  value = data.aws_subnet.private[*].id
+  value = aws_subnet.private[*].id
 }
 
 output "database_subnet_ids" {
-  value = data.aws_subnet.database[*].id
+  value = aws_subnet.database[*].id
 }
 
 
